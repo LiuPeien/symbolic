a2bede73c88d1002eaa3f5f1c861f4dfea30c95f
jinhongliu <jinhongliu@freewheel.tv>
2018-05-20 10:38:10 +0800

    version1 web server

---
+++ web/handle.py
@@ -0,0 +1,60 @@
+
+import hashlib
+import web
+import reply, receive
+
+
+class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = "liujinhong"   # 自己定义的tokent
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            sha1.update(''.join(list).encode('utf-8'))
+            hashcode = sha1.hexdigest()
+            print("handle/GET func: hashcode, signature:", hashcode, signature)
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception as e:
+            print(e)
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print
+            "Handle Post webdata is ", webData  # 后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg):
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                if recMsg.MsgType == 'text':
+                    content = "test"
+                    replyMsg = reply.TextMsg(toUser, fromUser, content)
+                    return replyMsg.send()
+                if recMsg.MsgType == 'image':
+                    mediaId = recMsg.MediaId
+                    replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
+                    return replyMsg.send()
+                else:
+                    return reply.Msg().send()
+            else:
+                print
+                "暂且不处理"
+                return reply.Msg().send()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    pass
